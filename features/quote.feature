b931be1352db66711a3f30edc43397b4b13ca2af
Jimi Odeyemi <jimode68@gmail.com>
2014-07-03 00:17:16 +0100

    initial commit

---
+++ features/quote.feature
@@ -0,0 +1,7 @@
+Feature: Quote
+
+Scenario: Get a BMW Car Insurance quote
+	Given I am on the BMW Car Insurance home page
+	When I click on the "Get a Quote" link
+	Then I should be taken to another window displaying "Step 1- Your car"
+	
---
+++ features/step_definitions/quote_steps.rb
@@ -0,0 +1,17 @@
+Given(/^I am on the BMW Car Insurance home page$/) do
+  visit 'http://www.bmw-carinsurance.co.uk/'
+end
+
+When(/^I click on the "(.*?)" link$/) do |arg1|
+  click_link 'Get a Quote'
+end
+
+Then(/^I should be taken to another window displaying "(.*?)"$/) do |arg1|
+  main_window = page.driver.browser.window_handle
+  windows = page.driver.browser.window_handles
+  windows.each do |window|
+  	if main_window != window
+  		new_window = window
+  	end
+  end
+end
